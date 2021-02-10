21a82da6133bf28f4cadfa5eec86f95217121193
Mathieu Dalle <mathieudalle93@gmail.com>
2021-02-10 19:06:56 +0100

    corrected tests

--- test/TennisTest.java
+++ test/TennisTest.java
@@ -6,8 +6,11 @@ public class TennisTest {
 
     @ParameterizedTest
     @CsvSource({
+            "0, 0, The game hasn't started yet",
+            "1, 1, Looks like both players have reached fifteen",
             "1, 2, Player 2 is leading thirty to fifteen",
             "0, 3, Player 2 is leading forty to love",
+            "0, 4, Player 2 is our winner",
             "3, 3, We have a deuce",
             "4, 3, Player 1 has gotten himself an advantage dear Nelson",
             "5, 3, Player 1 is our winner",
@@ -17,6 +20,6 @@ public class TennisTest {
             "20, 3, Looks like the game is rigged because that score is not possible"
     })
     void shouldReturnGivenSentence(int player1, int player2, String result) {
-        Assertions.assertEquals(Tennis.applyScore(player1, player2), result);
+        Assertions.assertEquals(result, Tennis.applyScore(player1, player2));
     }
 }

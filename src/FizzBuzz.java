e81717b818b4bfbe944de82785b51230b06281fc
Mathieu Dalle <mathieudalle93@gmail.com>
2021-02-10 17:54:50 +0100

    added implementation with passing tests

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,6 +1,16 @@
 public class FizzBuzz {
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+
     public static String convertFizzBuzz(Integer number) {
-        //TODO implement
-        return "o";
+        if (number % 15 == 0) {
+            return FIZZ + BUZZ;
+        } else if (number % 5 == 0) {
+            return BUZZ;
+        } else if (number % 3 == 0) {
+            return FIZZ;
+        } else {
+            return number.toString();
+        }
     }
 }

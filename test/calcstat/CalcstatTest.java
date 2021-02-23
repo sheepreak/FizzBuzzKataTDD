b9f3fdd05436c3a845c51fee15f3e536b6d92952
Mathieu Dalle <mathieudalle93@gmail.com>
2021-02-23 18:18:36 +0100

    [GREEN] minimum value step 2 (wrong because skipped red phase because of first test...)

--- test/calcstat/CalcstatTest.java
+++ test/calcstat/CalcstatTest.java
@@ -10,9 +10,18 @@ public class CalcstatTest {
     // Added test data from the summary
     private List<Integer> sequence1 = List.of(6, 9, 15, -2, 92, 11);
 
+    // Added test data from the summary
+    private List<Integer> sequence2 = List.of(6, 9, 15, -5, 105, 11);
+
     @Test
     void shouldHaveMinusTwoWhenMinimalValueOnSequence1() {
         Calcstat calcstat = new Calcstat();
         Assertions.assertEquals(-2, calcstat.getMinimalValue(sequence1));
+    }
+
+    @Test
+    void shouldHaveMinusFiveWhenMinimalValueOnSequence2() {
+        Calcstat calcstat = new Calcstat();
+        Assertions.assertEquals(-5, calcstat.getMinimalValue(sequence2));
     }
 }
